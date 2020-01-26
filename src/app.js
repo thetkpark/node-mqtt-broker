b74258ccae03708ebbeef595e64de2efc724690e
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2020-01-26 10:59:53 +0700

    update gitignore for webstorm and sqlite working by shitty

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const mosca = require('mosca')
 const express = require('express')
 const routers = require('./routers.js')
 const addPollutionData = require('./utils/google_sheet')
+const { addData } = require('./utils/sqlite')
 
 const app = express()
 const setting = {
@@ -23,22 +24,24 @@ server.on('clientDisconnected', client => {
 
 server.on('published', (packet, client) => {
 	if (/[a-z]/.test(packet.payload.toString())) return 0
-	const topic = packet.topic
-	if (/\w*\/DHT11/.test(topic)) {
-		const data = /(\d*) (\d*)/.exec(packet.payload.toString())
-		console.log(`Humidity: ${data[1]}`)
-		console.log(`Temp: ${data[2]}\n`)
-	}
-	if (/\w*\/PMS3003/.test(topic)) {
-		const value = /(\d*) (\d*) (\d*)/.exec(packet.payload.toString())
-		const data = {
-			timestamp: new Date(),
-			pm1: value[1],
-			pm25: value[2],
-			pm10: value[3]
-		}
-		addPollutionData(data)
-	}
+	// const topic = packet.topic
+	// if (/\w*\/DHT11/.test(topic)) {
+	// 	const data = /(\d*) (\d*)/.exec(packet.payload.toString())
+	// 	console.log(`Humidity: ${data[1]}`)
+	// 	console.log(`Temp: ${data[2]}\n`)
+	// }
+	// if (/\w*\/PMS3003/.test(topic)) {
+	const value = /(\d*) (\d*) (\d*)/.exec(packet.payload.toString())
+	console.log(packet.payload.toString())
+	// const data = {
+	// 	timestamp: new Date(),
+	// 	pm1: value[1],
+	// 	pm25: value[2],
+	// 	pm10: value[3]
+	// }
+	addData(value[1], value[2], value[3])
+	// addPollutionData(data
+	// }
 })
 
 routers(app)
--- src/routers.js
+++ src/routers.js
@@ -1,9 +1,20 @@
-const pms3003Record = require('./utils/google_sheet')
+// const pms3003Record = require('./utils/google_sheet')
+const { db } = require('./utils/sqlite')
 
 const routes = app => {
 	app.get('/', (req, res) => {
-		res.send(pms3003Record)
+		res.send('HEllo')
 	})
+
+	app.get('/api', (req, res) => {
+		const sql_select = `SELECT * FROM pms3003 ORDER BY timestamp DESC LIMIT 1`
+		db.all(sql_select, (err, rows) => {
+			if (err) console.error(err.message)
+			console.log('Successful select data')
+			console.log(rows)
+			res.send(rows[0])
+		})
+})
 }
 
 module.exports = routes
